6d6548a559af512f27a87ff2811b30a5c82f5147
Luiz Felipe Torres Santos <94366321+luiztorresgithub@users.noreply.github.com>
2021-11-24 17:00:32 -0300

    exercícios

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,31 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOF(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.valores} adicionado.`
+        lista.appendChild(item)
+    } else {
+        window.alert('Valor invalido ou já encontrado')
+    }
+}
